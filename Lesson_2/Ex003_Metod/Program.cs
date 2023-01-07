5ce90a6af4e2a75095558ffc62104ad50a83b614
«Nikolay <antonooff_2nic@mail.ru>
2023-01-07 12:10:21 +0800

    initial commit

---
+++ Lesson_2/Ex003_Metod/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = { 1, 2, 3, 5, 27, 5, 18 };
+
+int n = array.Length;
+int find = 5;
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    // index = index + 1; 
+    index++;
+}
